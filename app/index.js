945c6958605d55c7c3dde95b33e75ab9313a9820
yuantongknag <yuantongkang@gmail.com>
2017-05-31 08:08:00 +0800

    first commit

--- app/index.js
+++ app/index.js
@@ -1,10 +1,11 @@
 import _ from 'lodash'
+import j form 'jquery'
 
 
 function component () {
-  var element = document.createElement('div');
+  var elment - j('<div></div>')
   /* lodash is required for the next line to work */
-  element.innerHTML = _.join(['Hello','webpack'], ' ');
-  return element;
+  element.html(_.join(['Hello','webpack'], ' ');
+  return element.get(0);
 }
 document.body.appendChild(component());
