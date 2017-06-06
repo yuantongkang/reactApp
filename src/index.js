b8992470f6092ebc484cda69d7554f790d9afd55
Yuantong <yuantongkang@gmail.com>
2017-06-06 16:02:53 +0800

    lifeCycle

--- src/index.js
+++ src/index.js
@@ -2,7 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Welcome from './Welcome'
 ReactDOM.render(
   <h1>Hi, world!</h1>,
-  document.getElementById('root2')
+  document.getElementById('root')
 );
+/*function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(
+    element,
+    document.getElementById('root')
+  );
+}*/
+
+// setInterval(tick, 1000);
+
+
+// class Welcome extends React.Component{
+//   render(){
+//     return <h1>hello, {this.props.name}</h1>;
+//   }
+// }
+ReactDOM.render(<Welcome name = "YuanTong"/>,
+  document.querySelector('#root')
+)
+
+
+
+
+
