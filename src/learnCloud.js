b1a8d945b43a654906a60290fd52eb01ed5d7e44
Yuantong <yuantongkang@gmail.com>
2017-06-14 06:30:43 +0800

    修改UserDialog.js

---
+++ src/learnCloud.js
@@ -0,0 +1,8 @@
+import AV from 'leancloud-storage'
+var APP_ID = 'ngEhR7Y6r7y7CWYRGnsPlfzR-gzGzoHsz';
+var APP_KEY = 'i4WgHC2o1DqbijKUaVoTiDqe';
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
+export default AV
