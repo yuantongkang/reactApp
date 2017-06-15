fa43ac624cd8521562afd4f82f8685fbdc014bdf
Yuantong <yuantongkang@gmail.com>
2017-06-15 21:54:35 +0800

    注册成功后，记住 user，显示 username

--- src/learnCloud.js
+++
@@ -1,8 +0,0 @@
-import AV from 'leancloud-storage'
-var APP_ID = 'ngEhR7Y6r7y7CWYRGnsPlfzR-gzGzoHsz';
-var APP_KEY = 'i4WgHC2o1DqbijKUaVoTiDqe';
-AV.init({
-  appId: APP_ID,
-  appKey: APP_KEY
-});
-export default AV
