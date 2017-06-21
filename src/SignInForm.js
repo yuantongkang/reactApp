608446db331846261016a17b7e7d5affeb17372c
Yuantong <yuantongkang@gmail.com>
2017-06-21 18:29:42 +0800

    重构：抽离一些组件

---
+++ src/SignInForm.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+
+export default class SignInFrom extends Component{
+  render(){
+    return <form className="signIn" onSubmit={this.props.onSubmit}> {/* 登录*/}
+      <div className="row">
+        <label>用户名</label>
+        <input type="text" value={this.props.formData.username}
+          onChange={this.props.onChange.bind(null, 'username')} />
+      </div>
+      <div className="row">
+        <label>密码</label>
+        <input type="password" value={this.props.formData.password}
+          onChange={this.props.onChange.bind(null, 'password')} />
+      </div>
+      <div className="row actions">
+        <button type="submit">登录</button>
+        <a href="#" onClick={this.props.onForgotPassword}>忘记密码了？</a>
+      </div>
+    </form>
+  }
+
+}
+
+
+
+
